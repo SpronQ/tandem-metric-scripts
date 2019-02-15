33f1520311ebae16e2f4a7c96ae6039a0b1ef518
Han van Oostende <hvoostende@gmail.com>
2019-02-15 15:53:20 +0100

    Update recordDeploy.sh

--- recordDeploy.sh
+++ recordDeploy.sh
@@ -7,4 +7,5 @@ TIMESTAMP=`date -u +"%FT%T.000Z"`
 COMMITTER=`git log -1 --pretty=format:%ae`
 JSON_FMT='{\"ci_cd_id\":\"%s\",\"product_name\":\"%s\",\"timestamp\":\"%s\",\"git_commit\":\"%s\",\"committer\":\"%s\"}'
 EVENT=`printf "$JSON_FMT" "$WERCKER_RUN_ID" "$PRODUCT_NAME" "$TIMESTAMP" "$WERCKER_GIT_COMMIT" "$COMMITTER"`
-bash <(curl https://raw.githubusercontent.com/SpronQ/tandem-metric-scripts/master/recordDeploy.sh)
+# gcloud beta functions call ProcessTandemDeployMetrics --region europe-west1 --data $EVENT
+curl -X POST "https://europe-west1-tandem-metrics-229509.cloudfunctions.net/ProcessTandemDeployMetrics" -H "Content-Type:application/json" --data $EVENT
